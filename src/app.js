88df5cd313894e4c0d08f69dc27f97abab5e2c63
Amit Sarangi <amitsarangi@Amits-MacBook-Pro.local>
2021-09-27 13:37:16 +0530

    clean up

--- src/app.js
+++ src/app.js
@@ -1,40 +1,6 @@
-import * as THREE from 'three';
-
-
 export default class Sketch {
     constructor() {
         this.time = 0;
-
-        this.renderer = new THREE.WebGLRenderer( { antialias: true } );
-        this.renderer.setSize( window.innerWidth, window.innerHeight );
-        document.querySelector("#container").appendChild( this.renderer.domElement );
-
-        this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-        this.camera.position.z = 1;
-
-        this.scene = new THREE.Scene();
-
-        this.addMesh();
-
-        this.render();
-    }
-
-    addMesh() {
-        // this.geometry = new THREE.PlaneBufferGeometry(1, 1);
-        this.geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-        this.material = new THREE.MeshNormalMaterial();
-
-        this.mesh = new THREE.Mesh( this.geometry, this.material );
-        this.scene.add(this.mesh);
-    }
-
-    render() {
-        this.time++;    
-        console.log(this.time);
-        this.mesh.rotation.x = this.time / 2000;
-	    this.mesh.rotation.y = this.time / 1000;
-        this.renderer.render( this.scene, this.camera );
-        window.requestAnimationFrame(this.render.bind(this));
     }
 }
 
